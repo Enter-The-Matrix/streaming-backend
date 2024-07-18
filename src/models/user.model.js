3662f3f787ebd45d4560569c5ebb43229ea4a741
Enter-The-Matrix <ashwanisajwanpro@gmail.com>
2024-07-18 19:56:18 +0530

    jwt token and bcrypt and mongoose aggregate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,54 +1,103 @@
-import { Timestamp } from "mongodb";
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({
-
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String, //data type String
+      required: true, //always needed
+      unique: true, // should be unique in the database
+      lowercase: true, // turn the string to lowercase
+      trim: true, // remove space from start and end of string
+      index: true, // that improves the speed of data retrieval operations
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String,// cloudinary url,
-        required: true,
+      type: String, // cloudinary url,
+      required: true,
     },
     coverImage: {
-        type: String,// cloudinary url,
+      type: String, // cloudinary url,
     },
     watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Video'
-        }
+      // it been inside an array is equal to the data type array and each element has a below schema
+      {
+        type: Schema.Types.ObjectId, //the data type when using the other schema
+        ref: "Video", //the name of schema to be used
+      },
     ],
     password: {
-        type: String,
-        required: [true, 'Password is required']
+      type: String,
+      required: [true, "Password is required"], //custom msg when password is not provided
     },
     refreshToken: {
-        type: String
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
+
+// Pre hooks allow us to execute custom logic before a certain event occurs, such as before saving a
+// document to the database. Don't use an arrow function inside this pre hook
+// "save" is keyword and "password" also
+userSchema.pre("save", async function (next) {
+  // This is a Mongoose method that checks if the specified field ("password" in this case) has been modified
+  // since the document was loaded from the database.
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// "userSchema.methods" It is a way to add instance methods to a Mongoose schema.
+// password here is the password that has came from frontend
+// this.password is the one hashed password saved in the database
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// method created to generate a jwt token
+userSchema.methods.generateAccessToken = function () {
+  return (
+    jwt.sign({
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    }),
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
+  );
+};
 
-},
+// method created to generate a jwt refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return (
+    jwt.sign({
+      _id: this._id,
+    }),
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-    { timestamps: true }
-
-)
-
-export const User = mongoose.model('User', userSchema)
+export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,45 +1,47 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
-
+const videoSchema = new Schema(
+  {
     videoFile: {
-        type: String, //cloudinary url
-        required:true
+      type: String, //cloudinary url
+      required: true,
     },
-    thumbnail:{
-        type: String, //cloudinary url
-        required:true
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
     },
-    title:{
-        type: String, 
-        required:true
+    title: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String, 
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
-    duration:{
-        type: Number, //cloudinary url
-        required:true
+    duration: {
+      type: Number, //cloudinary url
+      required: true,
     },
-    views:{
-        type: Number, 
-        default:0
+    views: {
+      type: Number,
+      default: 0,
     },
-    isPublished:{
-        type: Boolean, 
-        default:true
+    isPublished: {
+      type: Boolean,
+      default: true,
     },
-    owner:{
-        type:Schema.Types.ObjectId,
-        ref:"User"
-    }
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},
-    {
-        timestamps: true
-    }
+// adding a plugin is a way to extend the functionality of our schemas
+videoSchema.plugin(mongooseAggregatePaginate);
 
-)
-
-export const Video = mongoose.model('Video', videoSchema)
+export const Video = mongoose.model("Video", videoSchema);
